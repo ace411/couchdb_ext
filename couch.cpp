f65c3cf3cffbe29c97821f2468e24dd11dbf88ad
ace411 <lochbm@live.com>
2019-08-16 09:16:52 +0300

    Modified if statement body

--- couch.cpp
+++ couch.cpp
@@ -393,7 +393,6 @@ PHP_METHOD(Request, _delete)
             case COUCH_DEL_DOC:
                 if (!zend_hash_exists(opts, idkey) || !zend_hash_exists(opts, revkey))
                 {
-                    zend_string_release(dbkey);
                     zend_throw_exception(request_exception_ce, "'_id' or '_rev' key is missing", 0 TSRMLS_CC);
                     RETURN_NULL();
                 }
